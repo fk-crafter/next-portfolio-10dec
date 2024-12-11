ae1f3de63c5b13fe9d8eccdb127104098e91ff7a
Fowad Khan <fowad.kh@outlook.fr>
2024-12-11 12:10:54 +0100

    initial commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,19 +3,22 @@ import { RxGithubLogo } from "react-icons/rx";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-transparent text-gray-200 shadow-lg p-5">
-      <div className="flex flex-col items-center justify-center">
-        {/* Lien GitHub */}
+    <div className="w-full bg-[#0f172a] text-gray-200 shadow-lg p-5 relative z-[20]">
+      <div className="flex flex-col items-center justify-center gap-3">
         <a
           href="https://github.com/fk-crafter"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 text-[18px] font-semibold hover:text-[#6e5494] transition-all duration-300"
-          style={{ cursor: "pointer" }} // Assure le curseur cliquable
+          style={{ cursor: "pointer" }}
         >
           <RxGithubLogo className="text-2xl" />
           <span>GitHub</span>
         </a>
+
+        <div className="text-sm text-gray-400 mt-2">
+          FK-Crafter | All Rights Reserved
+        </div>
       </div>
     </div>
   );
