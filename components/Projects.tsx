d793fb7b5929847fee0516e95f0771237640c932
Fowad Khan <fowad.kh@outlook.fr>
2024-12-17 16:14:27 +0100

    add gh graph

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import { projectData } from "@/constants";
 import { useLanguage } from "@/context/LanguageContext";
+import GitHubCalendar from "react-github-calendar";
 
 const cardAnimation = {
   hidden: { opacity: 0 },
@@ -43,53 +44,50 @@ const Projects = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-5 md:px-10">
         {projectData.map((project, index) => (
-          <a
-            key={index}
-            href={project.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="group"
-          >
-            <motion.div
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true }}
-              custom={index}
-              variants={cardAnimation}
-              className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]  transition-transform duration-300"
+          <div key={index}>
+            <a
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group"
             >
-              <Image
-                src={project.src}
-                alt={project.title}
-                width={1000}
-                height={1000}
-                className="w-full h-[200px] object-cover"
-              />
-
-              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <div className="flex gap-4">
-                  {project.technologies.map((tech, techIndex) => (
-                    <Image
-                      key={techIndex}
-                      src={tech}
-                      alt="Tech Icon"
-                      width={40}
-                      height={40}
-                      className="hover:scale-110 transition-transform duration-200"
-                    />
-                  ))}
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                custom={index}
+                variants={cardAnimation}
+                className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] transition-transform duration-300"
+              >
+                <Image
+                  src={project.src}
+                  alt={project.title}
+                  width={1000}
+                  height={1000}
+                  className="w-full h-[200px] object-cover"
+                />
+                <div className="p-4 bg-[#1a1c2b]">
+                  <h2 className="text-2xl font-semibold text-white">
+                    {project.title}
+                  </h2>
+                  <p className="mt-2 text-gray-300">{project.description}</p>
                 </div>
-              </div>
-
-              <div className="p-4 bg-[#1a1c2b]">
-                <h2 className="text-2xl font-semibold text-white">
-                  {project.title}
-                </h2>
-                <p className="mt-2 text-gray-300">{project.description}</p>
-              </div>
-            </motion.div>
-          </a>
+              </motion.div>
+            </a>
+          </div>
         ))}
+      </div>
+
+      <div className="mt-10">
+        <h3 className="text-white text-lg mb-2">
+          {isFrench ? "Mes Contributions" : "My Contributions"}
+        </h3>
+        <GitHubCalendar
+          username="fk-crafter"
+          blockSize={10}
+          blockMargin={5}
+          fontSize={14}
+        />
       </div>
     </section>
   );
